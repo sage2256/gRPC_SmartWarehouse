966cbf10ee4d93334a15ac3b4bddc1bbc74a6312
sage2256 <118544399+sage2256@users.noreply.github.com>
2023-04-17 16:56:25 +0100

    Smart lighting server implemented

--- smartWarehouse/src/main/java/grpc/smartlighting/smartwarehouse/SmartLightingServer.java
+++ smartWarehouse/src/main/java/grpc/smartlighting/smartwarehouse/SmartLightingServer.java
@@ -8,11 +8,12 @@ import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
 public class SmartLightingServer extends SmartLightingServiceImplBase{
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 SmartLightingServer LightingServer = new SmartLightingServer();
-		 int port = 50052;
+		 int port = 50053;
 		 
 		 Server server;
 		try {
@@ -27,23 +28,79 @@ public class SmartLightingServer extends SmartLightingServiceImplBase{
 		
 		 
 	}
-
+	private boolean isLightOn = false;
 	@Override
 	public StreamObserver<BrightnessLevel> brightness(StreamObserver<LightStatus> responseObserver) {
 		// TODO Auto-generated method stub
-		return super.brightness(responseObserver);
+		//return super.brightness(responseObserver);
+		 return new StreamObserver<BrightnessLevel>() {
+	            @Override
+	            public void onNext(BrightnessLevel value) {
+	                float brightnessLevel = value.getLevel();
+	                if (brightnessLevel < 0.0) {
+	                    brightnessLevel = 0.0f;
+	                } else if (brightnessLevel > 1.0) {
+	                    brightnessLevel = 1.0f;
+	                }
+	                System.out.println("Received brightness level: " + brightnessLevel);
+	                isLightOn = brightnessLevel > 0.5;
+	                responseObserver.onNext(LightStatus.newBuilder().setOn(isLightOn).build());
+	            }
+
+	            @Override
+	            public void onCompleted() {
+	                System.out.println("Brightness stream completed");
+	                responseObserver.onCompleted();
+	            }
+
+				@Override
+				public void onError(Throwable t) {
+					// TODO Auto-generated method stub
+					
+				}
+	        };
 	}
 
 	@Override
 	public StreamObserver<OccupancyStatus> switch_(StreamObserver<LightStatus> responseObserver) {
 		// TODO Auto-generated method stub
-		return super.switch_(responseObserver);
+		//return super.switch_(responseObserver);
+		return new StreamObserver<OccupancyStatus>() {
+            @Override
+            public void onNext(OccupancyStatus value) {
+                boolean isOccupied = value.getOccupied();
+                System.out.println("Received occupancy status: " + isOccupied);
+                isLightOn = isOccupied;
+                responseObserver.onNext(LightStatus.newBuilder().setOn(isLightOn).build());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Switch stream completed");
+                responseObserver.onCompleted();
+            }
+
+			@Override
+			public void onError(Throwable t) {
+				// TODO Auto-generated method stub
+				
+			}
+        };
 	}
 
 	@Override
 	public void remote(RemoteRequest request, StreamObserver<Confirmation> responseObserver) {
 		// TODO Auto-generated method stub
-		super.remote(request, responseObserver);
+		//super.remote(request, responseObserver);
+		 boolean turnOn = request.getOn();
+	        System.out.println("Received remote request to turn " + (turnOn ? "on" : "off") + " the light");
+	        
+	        Confirmation confirmation = Confirmation.newBuilder()
+	                .setMessage("The light has been turned " + (turnOn ? "on" : "off"))
+	                .build();
+
+	        responseObserver.onNext(confirmation);
+	        responseObserver.onCompleted();
 	}
 
 	
---
+++ smartWarehouse/src/main/java/grpc/trackstock/smartwarehouse/TraskStockServer.java
@@ -0,0 +1,14 @@
+package grpc.trackstock.smartwarehouse;
+
+public class TraskStockServer {
+
+	public TraskStockServer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
