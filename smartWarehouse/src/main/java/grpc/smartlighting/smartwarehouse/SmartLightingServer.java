5a12c99ac589262930fe2ac737b3e20e6ee70e89
sage2256 <118544399+sage2256@users.noreply.github.com>
2023-04-17 15:55:27 +0100

    Update SmartLightingServer.java

--- smartWarehouse/src/main/java/grpc/smartlighting/smartwarehouse/SmartLightingServer.java
+++ smartWarehouse/src/main/java/grpc/smartlighting/smartwarehouse/SmartLightingServer.java
@@ -1,10 +1,51 @@
 package grpc.smartlighting.smartwarehouse;
 
-public class SmartLightingServer {
+import java.io.IOException;
+
+import grpc.smartlighting.smartwarehouse.SmartLightingServiceGrpc.SmartLightingServiceImplBase;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+public class SmartLightingServer extends SmartLightingServiceImplBase{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 SmartLightingServer LightingServer = new SmartLightingServer();
+		 int port = 50052;
+		 
+		 Server server;
+		try {
+			server = ServerBuilder.forPort(port).addService(LightingServer).build().start();
+			System.out.println("Server started....");
+			server.awaitTermination();
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		 
 	}
+
+	@Override
+	public StreamObserver<BrightnessLevel> brightness(StreamObserver<LightStatus> responseObserver) {
+		// TODO Auto-generated method stub
+		return super.brightness(responseObserver);
+	}
+
+	@Override
+	public StreamObserver<OccupancyStatus> switch_(StreamObserver<LightStatus> responseObserver) {
+		// TODO Auto-generated method stub
+		return super.switch_(responseObserver);
+	}
+
+	@Override
+	public void remote(RemoteRequest request, StreamObserver<Confirmation> responseObserver) {
+		// TODO Auto-generated method stub
+		super.remote(request, responseObserver);
+	}
+
+	
 
 }
